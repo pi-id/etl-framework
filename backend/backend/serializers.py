760b91976233a772cb4878b086a65f2335f72b57
Josip Domazet <josip.domazet@fer.hr>
2020-09-09 09:54:20 +0200

    Uncommented serializer

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -58,13 +58,13 @@ class MetaObjectTaskSerializer(serializers.ModelSerializer):
         fields.append('source_object_name')
         fields.append('target_object_name')
         fields.append('task_name')
-        
-#class MetaObjectTypeSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = MetaObjectType
-#        fields = "__all__"
 
-        
+ 
+class MetaObjectTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MetaObjectType
+        fields = "__all__"
+
 
 class MetaAttributeSerializer(serializers.ModelSerializer):
     task_name = serializers.CharField(source = 'task_sid.task_name', read_only = True)
